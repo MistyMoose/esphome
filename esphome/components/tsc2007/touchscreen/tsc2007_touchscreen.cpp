3f72c681f5f250980e3960389e1ea92b3478c2a3
MistyMoose <a.tavakkolius@gmail.com>
2024-03-17 10:04:23 -0400

    fixed .cpp

--- esphome/components/tsc2007/touchscreen/tsc2007_touchscreen.cpp
+++ esphome/components/tsc2007/touchscreen/tsc2007_touchscreen.cpp
@@ -8,7 +8,7 @@ namespace tsc2007_touchscreen {
 
 static const char *TAG = "tsc2007_touchscreen";
 
-void TSC2007Component::setup() {
+void TSC2007Touchscreen::setup() {
   ESP_LOGCONFIG(TAG, "Setting up TSC2007 Touchscreen...");
   if (!this->tsc_.begin(this->address_, this->wire_)) {
     ESP_LOGE(TAG, "Failed to initialize TSC2007");
@@ -17,14 +17,14 @@ void TSC2007Component::setup() {
   }
 }
 
-void TSC2007Component::dump_config() {
+void TSC2007Touchscreen::dump_config() {
   LOG_I2C_DEVICE(this);
   if (this->is_failed()) {
     ESP_LOGE(TAG, "Communication with TSC2007 failed!");
   }
 }
 
-void TSC2007Component::update() {
+void TSC2007Touchscreen::update() {
   uint16_t x, y, z1, z2;
   if (this->tsc_.read_touch(&x, &y, &z1, &z2)) {
     this->publish_state(x, y, z1, z2);
